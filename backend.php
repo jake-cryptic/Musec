e40002abb0021b26fca64f20a9ff00384e45b034
Jake Mcneill <jakemcneill83@me.com>
2016-05-17 07:53:55 +0100

    First Commit

---
+++ backend.php
@@ -0,0 +1,100 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: X-PINGOTHER, Content-Type");
+header("Access-Control-Max-Age: 86400");
+//header("HTTP/1.1 418 I'm a teapot");
+session_start();
+
+if (isset($_POST)) {
+	if (isset($_POST["t"])) {
+		$type = trim($_POST["t"]);
+	} else {
+		die('{"response":"error","error":"The backend could not understand your request"}');
+	}
+	if (isset($_POST["d"])) {
+		$dir = base64_decode($_POST["d"]);
+	} else {
+		$dir = "";
+	}
+	
+	if ($type == "f") {
+		// Folders (List directories of music)
+		$dirs = array_filter(glob('resources/music/*'), 'is_dir');
+		$data = '{"response":"lsdir","data":[';
+		foreach ($dirs as $directory) {
+			if (end($dirs) == $directory) {
+				$data .= '"' . str_replace("resources/music/","",$directory) . '"';
+			} else {
+				$data .= '"' . str_replace("resources/music/","",$directory) . '",';
+			}
+		}
+		echo "$data]}";
+	} elseif ($type == "s") {
+		// Song (List songs in directory)
+		$allowedExtensions = ["mp3","m4a"];
+		
+		$_SESSION["folder"] = $dir;
+		$files = scandir("resources/music/" . $dir);
+		$data = '{"response":"lsfiles","data":[';
+		foreach ($files as $file) {
+			if ($file != "." && $file != "..") {
+				$fileExtArr = explode(".",$file);
+				$fileExt = end($fileExtArr);
+				if (in_array($fileExt,$allowedExtensions)) {
+					if (end($files) == $file) {
+						$data .= '"' . $dir . "/" . $file . '"';
+					} else {
+						$data .= '"' . $dir . "/" . $file . '",';
+					}
+				}
+			}
+		}
+		sleep(1);
+		echo "$data]}";
+	} elseif ($type == "l") {
+		// Look (Search for song in all folders)
+		$forbiddenSearches = ["mp3","m4a"];
+		$it = new RecursiveDirectoryIterator("resources/music/",RecursiveDirectoryIterator::SKIP_DOTS);
+		
+		if (isset($_POST["s"])){
+			$searchTerm = htmlspecialchars(stripslashes(trim(base64_decode($_POST["s"]))));
+			$searchTerm = strtolower($searchTerm);
+			
+			if (in_array($searchTerm,$forbiddenSearches)){
+				die('{"response":"error","error":"Invalid Search"}');
+			}
+		} else {
+			die('{"response":"error","error":"Search Failed"}');
+		}
+		
+		$count = 0;
+		$results = array("response"=>"sresult");
+		$currentdir = getcwd();
+		if (strpos($currentdir,"\\") !== false) {
+			$xplChar = "\\";
+		} else {
+			$xplChar = "/";
+		}
+		foreach(new RecursiveIteratorIterator($it) as $f) {
+			$dataArray = explode($xplChar,$f);
+			
+			$file = $dataArray[count($dataArray)-1];
+			$dir = $dataArray[count($dataArray)-2];
+			
+			if (strpos(strtolower($file),$searchTerm) !== false) {
+				$count++;
+				$results["r"][$count] = array($file,$dir);
+			}
+		}
+		sleep(1);
+		$results["count"] = $count;
+		echo @json_encode($results);
+	} else {
+		die('{"response":"error","error":"The backend cannot process your request"}');
+	}
+} else {
+	die('{"response":"error","error":"No data sent"}');
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,70 @@
+<?php session_start(); ?>
+<!DOCTYPE HTML>
+<html lang="en" dir="ltr">
+	<head>
+		
+		<!-- Title and Metadata -->
+		<title>Loading</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="author" content="" />
+		<meta name="google" content="notranslate" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		
+		<!-- Styles and Graphics -->
+		<link rel="stylesheet" type="text/css" href="assets/css/global.css" media="screen" />
+		<link rel="apple-touch-icon" sizes="250x250" href="assets/img/Musec!3.jpg">
+		<link rel="icon" href="assets/img/Musec!.jpg" type="image/gif">
+		
+	</head>
+	<body>
+		<!--[if lte IE 8]>
+			<h1 style="text-align:center;font-size:2.2em;">Browser Not Supported</h1>
+			<h2 style="text-align:center;font-size:1.5em;">Please download a better browser</h2>
+			<a href="https://www.google.com/chrome/">Chrome</a> | <a href="https://mozilla.org/firefox/">Firefox</a> | <a href="https://www.opera.com/">Opera</a>
+			<div style="display:none">
+		<!--<![endif]-->
+		<div id="pageTop">
+			<span id="back" do="refresh">&#x21bb;</span>
+			<span id="search">&#x1f50e;</span>
+			<span id="queue" do="showQ">&#9776;</span>
+			<div id="search_container"><input type="text" id="search_box" placeholder="Search for a song" /><button id="do_search">Search All</button></div>
+			<span id="folder">Please Wait</span>
+		</div>
+		<div id="pageCenter">
+			<div id="musicFolders">
+				<h1>Loading Content...</h1>
+			</div>
+			<div id="songFolder">
+				<h1>Loading Songs...</h1>
+			</div>
+			<div id="queueFolder">
+				<h1>Just a second...</h1>
+			</div>
+			<div id="searchFolder">
+				<h1>Checking For Songs...</h1>
+			</div>
+		</div>
+		<div id="pageBottom">
+			<span id="playpause" class="disabled">&#9658;</span>
+			<span id="mediacontrols">No Music Playing</span>
+		</div>
+		
+		<!-- Scripts and Libraries -->
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"></script>
+		<script type="text/javascript" src="assets/js/jquery.longclick-min.js"></script>
+		<script type="text/javascript" src="assets/polyfills/modernizr-custom.js"></script>
+		<script type="text/javascript">
+			console.log("Musec: Loading...");
+			if (typeof(jQuery) == "undefined"){document.write('<script src="assets/libs/jquery-2.1.4.min.js" type="text/javascript"><\/script>');}
+			if (typeof(FastClick) == "undefined"){document.write('<script src="assets/assets/js/fastclick.js" type="text/javascript"><\/script>');}
+			if (!Modernizr.atobbtoa){document.write('<script type="text/javascript" src="assets/polyfills/base64.min.js"><\/script>');}
+			if (!Modernizr.json){document.write('<script type="text/javascript" src="assets/polyfills/json3.min.js"><\/script>');}
+			function isNumeric(n){return !isNaN(parseFloat(n)) && isFinite(n);}
+			function capitalise(t){return t.replace(/\w\S*/g, function(s){return s.charAt(0).toUpperCase() + s.substr(1).toLowerCase();});}
+		</script>
+		<script type="text/javascript" src="assets/js/tiles2.js"></script>
+	</body>
+</html>
